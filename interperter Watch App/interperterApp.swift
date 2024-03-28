2f9e9e2e972abe8ce242ee6ff9f29d12e5b3f737
Danny Ruchtie <druchtie@gmail.com>
2024-03-28 11:34:20 +0100

    Initial Commit

---
+++ interperter Watch App/interperterApp.swift
@@ -0,0 +1,17 @@
+//
+//  interperterApp.swift
+//  interperter Watch App
+//
+//  Created by Danny Ruchtie on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct interperter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
