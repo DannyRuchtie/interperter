864beecdf7ef36c130b3bb7e1f67fef358f39e64
Danny Ruchtie <druchtie@gmail.com>
2024-03-28 19:47:10 +0100

    Revert "version 0.1"
    This reverts commit 209b5e242082f5681d73fbe69b58f0da8b33bb43.

--- interperter Watch App/ContentView.swift
+++ interperter Watch App/ContentView.swift
@@ -1,40 +1,24 @@
+//
+//  ContentView.swift
+//  interperter Watch App
+//
+//  Created by Danny Ruchtie on 28/03/2024.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputText = ""
-
     var body: some View {
         VStack {
-            TextField("Enter text", text: $inputText)
-                .padding()
-
-            Button("Send") {
-                sendInputToMac(inputText)
-            }
-            .padding()
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
+}
 
-    func sendInputToMac(_ text: String) {
-        guard let url = URL(string: "http://127.0.0.1:10001") else { return }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let body: [String: Any] = ["text": text] // Change 'input' to 'text'
-        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                // Handle the response from your Mac
-                print(String(data: data, encoding: .utf8) ?? "")
-            } else if let error = error {
-                print("HTTP Request Failed \(error)")
-            }
-            DispatchQueue.main.async {
-                self.inputText = "" // Clear the input field
-            }
-        }.resume()
-
-    }
+#Preview {
+    ContentView()
 }
